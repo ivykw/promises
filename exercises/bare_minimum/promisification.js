bea9f78c269e4ee10c32b9a4511583a929396d36
ivykw <ivyw219@gmail.com>
2022-03-15 14:08:17 -0500

    Complete bare_minimum/promisification.js exercises

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('needle');
@@ -29,7 +29,7 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
 
 
 // (2) Asyncronous token generation
@@ -40,14 +40,14 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken); // TODO
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
     if (err) { return callback(err); }
-   
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
@@ -57,8 +57,34 @@ var readFileAndMakeItFunny = function(filePath, callback) {
     callback(funnyFile);
   });
 };
+// it('should make a funny file available in the `then` block', function(done) {
+//   readFileAndMakeItFunnyAsync(__dirname + '/../files/file_to_read.txt')
+//     .then(function(funnyFile) {
+//       funnyFile.split('\n').forEach(function(line) {
+//         expect(line).to.contain('lol');
+//       });
+//       done();
+//     })
+//     .catch(done);
+// });
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = function(filePath) {
+  return new Promise((resolve, reject) =>{
+    fs.readFile(filePath, 'utf8', function(err, file) {
+      if (err) {
+        reject(err);
+      } else {
+        var funnyFile = file.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
+        resolve(funnyFile);
+      }
+    });
+  });
+
+}; // TODO
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
